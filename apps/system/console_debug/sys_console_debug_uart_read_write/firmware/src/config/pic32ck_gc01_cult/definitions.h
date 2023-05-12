671983d5e6be4b4ff3487608b96395a1de943d89
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-12 10:12:55 +0530

    [SYS_CONSOLE] Regenerated demo for system_console_debug_sys_console_debug_uart_read_write_pic32ck_gc01_cult
    MH3-75901

--- apps/system/console_debug/sys_console_debug_uart_read_write/firmware/src/config/pic32ck_gc01_cult/definitions.h
+++ apps/system/console_debug/sys_console_debug_uart_read_write/firmware/src/config/pic32ck_gc01_cult/definitions.h
@@ -54,8 +54,8 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
-#include "peripheral/sercom/usart/plib_sercom5_usart.h"
 #include "peripheral/pm/plib_pm.h"
+#include "peripheral/sercom/usart/plib_sercom5_usart.h"
 #include "system/console/sys_console.h"
 #include "system/console/src/sys_console_uart_definitions.h"
 #include "system/int/sys_int.h"
@@ -73,6 +73,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32CK2051GC01144"
+#define DEVICE_ARCH			 "CORTEX-M33"
+#define DEVICE_FAMILY		 "PIC32CK"
+#define DEVICE_SERIES		 "PIC32CKGC01"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 120000000
