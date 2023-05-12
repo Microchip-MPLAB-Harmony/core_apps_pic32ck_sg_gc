3d0f056cb01de5bdd127a961e73567167057a1c1
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-12 10:12:56 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_pic32ck_gc01_cult
    MH3-75902

--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32ck_gc01_cult/definitions.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32ck_gc01_cult/definitions.h
@@ -58,11 +58,11 @@
 #include "peripheral/pm/plib_pm.h"
 #include "peripheral/sercom/usart/plib_sercom5_usart.h"
 #include "peripheral/rtc/plib_rtc.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -74,6 +74,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32CK2051GC01144"
+#define DEVICE_ARCH			 "CORTEX-M33"
+#define DEVICE_FAMILY		 "PIC32CK"
+#define DEVICE_SERIES		 "PIC32CKGC01"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 120000000
