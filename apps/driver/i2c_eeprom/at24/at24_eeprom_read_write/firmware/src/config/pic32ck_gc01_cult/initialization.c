fac4aa483878be18338c53927ceea1b2301ecf10
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-12 10:12:39 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_eeprom_at24_at24_eeprom_read_write_pic32ck_gc01_cult
    MH3-75888

--- apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/pic32ck_gc01_cult/initialization.c
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/pic32ck_gc01_cult/initialization.c
@@ -325,19 +325,23 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
 // <editor-fold defaultstate="collapsed" desc="DRV_AT24 Initialization Data">
 
 /* I2C PLIB Interface Initialization for AT24 Driver */
-const DRV_AT24_PLIB_INTERFACE drvAT24PlibAPI = {
+static const DRV_AT24_PLIB_INTERFACE drvAT24PlibAPI = {
 
     /* I2C PLIB WriteRead function */
     .writeRead = (DRV_AT24_PLIB_WRITE_READ)SERCOM4_I2C_WriteRead,
 
     /* I2C PLIB Write function */
-    .write = (DRV_AT24_PLIB_WRITE)SERCOM4_I2C_Write,
+    .write_t = (DRV_AT24_PLIB_WRITE)SERCOM4_I2C_Write,
 
     /* I2C PLIB Read function */
-    .read = (DRV_AT24_PLIB_READ)SERCOM4_I2C_Read,
+    .read_t = (DRV_AT24_PLIB_READ)SERCOM4_I2C_Read,
 
     /* I2C PLIB Transfer Status function */
     .isBusy = (DRV_AT24_PLIB_IS_BUSY)SERCOM4_I2C_IsBusy,
@@ -350,7 +354,7 @@ const DRV_AT24_PLIB_INTERFACE drvAT24PlibAPI = {
 };
 
 /* AT24 Driver Initialization Data */
-const DRV_AT24_INIT drvAT24InitData =
+static const DRV_AT24_INIT drvAT24InitData =
 {
     /* I2C PLIB API  interface*/
     .i2cPlib = &drvAT24PlibAPI,
@@ -369,8 +373,8 @@ const DRV_AT24_INIT drvAT24InitData =
 
     .blockStartAddress =    0x0,
 };
-
 // </editor-fold>
+
 
 
 // *****************************************************************************
@@ -402,7 +406,7 @@ SYSTEM_OBJECTS sysObj;
 // *****************************************************************************
 // *****************************************************************************
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -435,17 +439,26 @@ void SYS_Initialize ( void* data )
     SERCOM4_I2C_Initialize();
 
 
+
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
     sysObj.drvAT24 = DRV_AT24_Initialize(DRV_AT24_INDEX, (SYS_MODULE_INIT *)&drvAT24InitData);
 
 
 
 
+    /* MISRAC 2012 deviation block end */
     APP_Initialize();
 
 
     NVIC_Initialize();
 
+
     /* MISRAC 2012 deviation block end */
+
 }
 
 
