78d9f20fe6ff74fc03eda4b22355d63861055bb7
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2026-09-15 11:39:54 +0530

    [DRV_USART] Regenerated demo for apps__driver__usart__sync__usart_echo__firmware__pic32ck_gc01_cult_freertos.X
    MH3C-1103

--- apps/driver/usart/sync/usart_echo/firmware/src/config/pic32ck_gc01_cult_freertos/initialization.c
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/pic32ck_gc01_cult_freertos/initialization.c
@@ -97,8 +97,6 @@
 #pragma config FUSES_USERCFG1_FUCFG2_BRCFGUDSSMOR_BOR_TRIP = 0x3U
 #pragma config FUSES_USERCFG1_FUCFG3_HYST_BOR_VDDIO = SET
 #pragma config FUSES_USERCFG1_FUCFG3_BOR_TRIP_VDDIO = 0x3U
-#pragma config FUSES_USERCFG1_FUCFG3_HYST_BOR_VDDIOB = SET
-#pragma config FUSES_USERCFG1_FUCFG3_BOR_TRIP_VDDIOB = 0x3U
 #pragma config FUSES_USERCFG1_FUCFG3_HYST_BOR_VDDA = SET
 #pragma config FUSES_USERCFG1_FUCFG3_BOR_TRIP_VDDA = 0x3U
 #pragma config FUSES_USERCFG1_FUCFG3_HYST_BOR_VDDREG = SET
@@ -228,8 +226,6 @@
 #pragma config FUSES_USERCFG2_FUCFG2_BRCFGUDSSMOR_BOR_TRIP = 0x3U
 #pragma config FUSES_USERCFG2_FUCFG3_HYST_BOR_VDDIO = SET
 #pragma config FUSES_USERCFG2_FUCFG3_BOR_TRIP_VDDIO = 0x3U
-#pragma config FUSES_USERCFG2_FUCFG3_HYST_BOR_VDDIOB = SET
-#pragma config FUSES_USERCFG2_FUCFG3_BOR_TRIP_VDDIOB = 0x3U
 #pragma config FUSES_USERCFG2_FUCFG3_HYST_BOR_VDDA = SET
 #pragma config FUSES_USERCFG2_FUCFG3_BOR_TRIP_VDDA = 0x3U
 #pragma config FUSES_USERCFG2_FUCFG3_HYST_BOR_VDDREG = SET
@@ -325,10 +321,10 @@
 // *****************************************************************************
 // *****************************************************************************
 /* Following MISRA-C rules are deviated in the below code block */
-/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
-/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
-/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
-/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+/* MISRA C-2023 Rule 7.2 - Deviation record ID - H3_MISRAC_2023_R_7_2_DR_1 */
+/* MISRA C-2023 Rule 11.1 - Deviation record ID - H3_MISRAC_2023_R_11_1_DR_1 */
+/* MISRA C-2023 Rule 11.3 - Deviation record ID - H3_MISRAC_2023_R_11_3_DR_1 */
+/* MISRA C-2023 Rule 11.8 - Deviation record ID - H3_MISRAC_2023_R_11_8_DR_1 */
 // <editor-fold defaultstate="collapsed" desc="DRV_USART Instance 0 Initialization Data">
 
 static DRV_USART_CLIENT_OBJ drvUSART0ClientObjPool[DRV_USART_CLIENTS_NUMBER_IDX0];
@@ -408,7 +404,7 @@ SYSTEM_OBJECTS sysObj;
 // *****************************************************************************
 // *****************************************************************************
 
-/* MISRAC 2012 deviation block end */
+/* MISRAC 2023 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -423,8 +419,8 @@ SYSTEM_OBJECTS sysObj;
 void SYS_Initialize ( void* data )
 {
 
-    /* MISRAC 2012 deviation block start */
-    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+    /* MISRAC 2023 deviation block start */
+    /* MISRA C-2023 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2023_R_2_2_DR_1 */
 
   
     PORT_Initialize();
@@ -437,29 +433,29 @@ void SYS_Initialize ( void* data )
     EVSYS_Initialize();
 
 	BSP_Initialize();
-    PM_Initialize();
-
     SERCOM5_USART_Initialize();
 
+    PM_Initialize();
 
-    /* MISRAC 2012 deviation block start */
+
+    /* MISRAC 2023 deviation block start */
     /* Following MISRA-C rules deviated in this block  */
-    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
-    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+    /* MISRA C-2023 Rule 11.3 - Deviation record ID - H3_MISRAC_2023_R_11_3_DR_1 */
+    /* MISRA C-2023 Rule 11.8 - Deviation record ID - H3_MISRAC_2023_R_11_8_DR_1 */
 
     sysObj.drvUsart0 = DRV_USART_Initialize(DRV_USART_INDEX_0, (SYS_MODULE_INIT *)&drvUsart0InitData);
 
 
 
 
-    /* MISRAC 2012 deviation block end */
+    /* MISRAC 2023 deviation block end */
     APP_USART_ECHO_Initialize();
 
 
     NVIC_Initialize();
 
 
-    /* MISRAC 2012 deviation block end */
+    /* MISRAC 2023 deviation block end */
 }
 
 /*******************************************************************************
